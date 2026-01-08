fde9cc978d4678fcefaea475e9bb954ceae73243
lourenço <lourencochantre@icloud.com>
2026-01-08 10:49:43 +0000

    adicionei message box banana

--- Knife Hit/Form1.vb
+++ Knife Hit/Form1.vb
@@ -2,5 +2,6 @@
     Private Sub BtnSettings_Click(sender As Object, e As EventArgs) Handles BtnSettings.Click
         Me.Hide()
         Settings.Show()
+        MessageBox.Show("banana")
     End Sub
 End Class
